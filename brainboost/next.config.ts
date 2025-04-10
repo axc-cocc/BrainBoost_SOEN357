d153b8d922eb49c76b25ec657870d28dd61c29a4
Simon Kallassi <98189147+SimonKallassi@users.noreply.github.com>
2025-04-09 22:14:25 -0400

    Setup project structure, added missing packages, Tailwind config fixed

--- brainboost/next.config.ts
+++ brainboost/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+// next.config.ts
+const nextConfig = {
+    webpack(config: any) {
+        return config;
+    },
 };
 
 export default nextConfig;
